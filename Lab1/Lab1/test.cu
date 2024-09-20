0b4052547836dbf15a28de1b225dce2e0f0ef2c4
mathWay311 <matvej0311@gmail.com>
2024-09-20 23:24:57 +0700

    Update test.cu

--- Lab1/Lab1/test.cu
+++ Lab1/Lab1/test.cu
@@ -7,10 +7,13 @@ TEST(SumTest, OnePlusOneEqualsTwoCPU) {
 }
 
 TEST(SumTest, OnePlusOneEqualsTwoCUDA) {
-	float a[32], b[32];
+	__managed__ float a[32]; 
+	__managed__ float b[32];
+	__managed__ float c[32];
+	
 	a[0] = 1.f;
 	b[0] = 1.f;
-	float c[32];
+	
 	sum<<<1, 32>>>(a, b, c);
 	cudaDeviceSynchronize();
 	
