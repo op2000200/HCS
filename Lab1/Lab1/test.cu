956b51f5804f6b32fb702fa779c35eff33fe300f
mathWay311 <matvej0311@gmail.com>
2024-09-20 23:42:42 +0700

    Added gpu test

--- Lab1/Lab1/test.cu
+++ Lab1/Lab1/test.cu
@@ -28,6 +28,20 @@ TEST(CUDATest, OnePlusOneEqualsTwo) {
 	EXPECT_FLOAT_EQ(c[0], 2.f);
 }
 
+__managed__ float a2[4] {1.0f, 2.0f, 3.0f, 4.0f}; 
+__managed__ float b2[4] {5.0f, 6.0f, 7.0f, 8.0f};
+__managed__ float c2[4];
+
+TEST(CUDATest, ArrTest) {
+	sum<<<1, 4>>>(a2, b2, c2);
+	cudaDeviceSynchronize();
+	
+	EXPECT_FLOAT_EQ(c2[0], 6.f);
+	EXPECT_FLOAT_EQ(c2[1], 8.f);
+	EXPECT_FLOAT_EQ(c2[2], 10.f);
+	EXPECT_FLOAT_EQ(c2[3], 12.f);
+}
+
 
 int main(int argc, char *argv[]) {
 	::testing::InitGoogleTest(&argc, argv);
