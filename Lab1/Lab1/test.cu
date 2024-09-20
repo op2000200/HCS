66e339e1bb92ed1783902577a01ab924b3f7079c
mathWay311 <matvej0311@gmail.com>
2024-09-20 23:14:31 +0700

    Update test.cu

--- Lab1/Lab1/test.cu
+++ Lab1/Lab1/test.cu
@@ -7,11 +7,11 @@ TEST(SumTest, OnePlusOneEqualsTwoCPU) {
 }
 
 TEST(SumTest, OnePlusOneEqualsTwoCUDA) {
-	float* a[1], b[1];
+	float a[32], b[32];
 	a[0] = 1.f;
 	b[0] = 1.f;
-	float* c[1];
-	sum<<<1, 1>>>(a, b, c);
+	float c[32];
+	sum<<<1, 32>>>(a, b, c);
 	EXPECT_EQ(c[0], 2.f);
 }
 
