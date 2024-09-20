eaf0908b06f04d6e5c68e637c4c4d089ce2313e4
mathWay311 <matvej0311@gmail.com>
2024-09-20 21:58:30 +0700

    Update test.cu

--- Lab1/Lab1/test.cu
+++ Lab1/Lab1/test.cu
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
+TEST(SumTest, OnePlusOneEqualsTwo) {
+  EXPECT_EQ(sumH(1,1), 2);
+}
+
 
 int main(int argc, char *argv[]) {
 	::testing::InitGoogleTest(&argc, argv);
