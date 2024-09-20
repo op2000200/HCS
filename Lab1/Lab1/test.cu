e64641c4c012d06c99ad45034ac8842509e4b663
mathWay311 <matvej0311@gmail.com>
2024-09-20 22:31:51 +0700

    Update test.cu

--- Lab1/Lab1/test.cu
+++ Lab1/Lab1/test.cu
@@ -6,6 +6,12 @@ TEST(SumTest, OnePlusOneEqualsTwo) {
   EXPECT_EQ(sumH(1,1), 2);
 }
 
+TEST(SumTest, OnePlusOneEqualsTwo) {
+	const float* a = 1, b = 1, c = 0;
+	sum(1,1);
+	EXPECT_EQ(c, 2);
+}
+
 
 int main(int argc, char *argv[]) {
 	::testing::InitGoogleTest(&argc, argv);
