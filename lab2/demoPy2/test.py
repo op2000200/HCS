c3ea745e5d3c88893bb376a0238f1af704677e66
mathWay311 <matvej0311@gmail.com>
2024-10-20 23:09:52 +0700

    Test suite 1

--- lab2/demoPy2/test.py
+++ lab2/demoPy2/test.py
@@ -2,26 +2,14 @@ import torch
 import lab2
 
 
-vec1 = torch.empty(5)
-vec2 = torch.empty(5)
-vec3 = torch.empty(5)
-vec4 = torch.empty(5)
-
-for i in range(5):
-    vec1[i] = i
-    vec2[i] = i + 1
-    vec3[i] = i + 2
-    vec4[i] = i + 3
-print(vec1)
-print(vec2)
-print(vec3)
-print(vec4)
-res = lab2.gpu(vec1,vec2,vec3,vec4)
-print(res)
+# Assertion for test case 1
+def test_1_assertion(res):
+    return (res[0] == 3) and (res[1] == 11) and (res[2] == 23) and (res[3] == 39) and (res[4] == 59)
 
 
-
-def test1():
+# Test case 1
+# CPU
+def test_cpu_1():
     vec1 = torch.empty(5)
     vec2 = torch.empty(5)
     vec3 = torch.empty(5)
@@ -32,11 +20,26 @@ def test1():
         vec2[i] = i + 1
         vec3[i] = i + 2
         vec4[i] = i + 3
-    print(vec1)
-    print(vec2)
-    print(vec3)
-    print(vec4)
-    res = lab2.test(vec1,vec2,vec3,vec4)
-    print(res)
-    assert 1 == 1
+
+    res = lab2.cpu(vec1,vec2,vec3,vec4)
+
+    assert test_1_assertion(res)
+
+# Test case 1
+# GPU
+def test_gpu_1():
+    vec1 = torch.empty(5)
+    vec2 = torch.empty(5)
+    vec3 = torch.empty(5)
+    vec4 = torch.empty(5)
+
+    for i in range(5):
+        vec1[i] = i
+        vec2[i] = i + 1
+        vec3[i] = i + 2
+        vec4[i] = i + 3
+
+    res = lab2.gpu(vec1,vec2,vec3,vec4)
+
+    assert test_1_assertion(res)
 
