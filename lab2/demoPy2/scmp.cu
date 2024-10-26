4eff30ab63a5c7142c12de6d64757b5fc7d37303
op2000200 <op2000200@yandex.ru>
2024-10-26 17:41:37 +0700

    fixed??

--- lab2/demoPy2/scmp.cu
+++ lab2/demoPy2/scmp.cu
@@ -9,16 +9,12 @@ __global__ void scmpOnGPU(float* vector_a, float* vector_b, int size, float* res
     *result = vector_a[i] * vector_b[i];
 }
 
-__host__ void scmpOnCPU(const float** vector_a, const float** vector_b, int size, float* result, int n)
+__host__ void scmpOnCPU(const float* vector_a, const float* vector_b, int size, float* result, int n)
 {
+    *result = 0;
     for (int i = 0; i < n; i++)
     {
-        float buffer = 0;
-        for (int j = 0; j < size; j++)
-        {
-            buffer += vector_a[i][j] * vector_b[i][j];
-        }
-        result[i] = buffer;
+        *result += vector_a[i] +vector_b[i];
     }
 }
 
@@ -52,10 +48,15 @@ float calcOnGpu(torch::Tensor vec1, torch::Tensor vec2)
     scmpOnGPU <<<bl, th >>> (d_vector_a, d_vector_b, size, d_result);
 
     cudaDeviceSynchronize();
-    
-    float res = *d_result;
-    std::cout << res << std::endl;
-    return res;
+
+    // Копируем результат обратно на хост
+    cudaMemcpy(&buf, d_result, sizeof(float), cudaMemcpyDeviceToHost);
+
+    // Освобождаем память на устройстве
+    cudaFree(d_vector_a);
+    cudaFree(d_vector_b);
+    cudaFree(d_result);
+    return buf;
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
