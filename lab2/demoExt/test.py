cacc59a68814108fc29fcb09aa0b1b4190fb5ce4
mathWay311 <matvej0311@gmail.com>
2024-11-18 14:17:40 +0700

    Added  tests for lab3

--- lab2/demoExt/test.py
+++ lab2/demoExt/test.py
@@ -1,14 +1,34 @@
 import torch
-import lab2
+import lab3
+import unittest
 
-vec1 = torch.empty(5)
-vec2 = torch.empty(5)
 
-for i in range(5):
-    vec1[i] = i
-    vec2[i] = i + 1
+class TestCUDALinearLayer(unittest.TestCase):
 
-print(vec1)
-print(vec2)
-res = lab2.dot_gpu(vec1,vec2)
-print(res)
+
+    # Test case 1
+    # GPU
+    def test_gpu(self):
+        m = 2
+        k = 2
+        n = 1
+
+        X = torch.randn(m, k).cuda()
+        W = torch.randn(n, k).cuda()
+        b = torch.randn(n).cuda()
+
+        lib_res = lab3.linear_layer_calc_result(X, W, b)
+
+        linear_layer = torch.nn.Linear(k, n).cuda()
+
+        with torch.no_grad():
+            linear_layer.weight.copy_(W)
+            linear_layer.bias.copy_(b)
+
+        torch_res = linear_layer(X)
+
+        self.assertTrue(torch.allclose(lib_res, torch_res))
+
+
+if __name__ == '__main__':
+    unittest.main()
